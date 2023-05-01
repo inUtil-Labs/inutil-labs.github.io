af0efc82b0d90781ce829e3ea97bd9797049c2f2
alsak0de <63293958+alsak0de@users.noreply.github.com>
2023-05-01 10:52:35 +0200

    Update formex.js

--- assets/js/formex.js
+++ assets/js/formex.js
@@ -1,79 +1,35 @@
-window.onload = function() {
-  var phoneInput = document.getElementById('phone');
-  var whatsappCheckbox = document.getElementById('whatsapp');
-  var picturePreview = document.getElementById('picture-preview');
-  var uploadButton = document.getElementById('upload-button');
+// Get references to the relevant elements
+const form = document.querySelector("#person-details");
+const whatsappCheckbox = document.querySelector("#use-whatsapp-image");
+const whatsappNumber = document.querySelector("#mobile-phone");
+const fileInput = document.querySelector("#picture-upload");
+const previewImage = document.querySelector("#picture-preview");
 
-  // Check if the phone number is valid
-  function isPhoneNumberValid(phoneNumber) {
-    var regex = /^\d+$/;
-    return regex.test(phoneNumber);
+// Function to handle the "Use Whatsapp Image" checkbox
+function handleWhatsappImage() {
+  if (whatsappCheckbox.checked && /^\d+$/.test(whatsappNumber.value)) {
+    const url = `https://haproxy.inutil-labs.com/wspic/number=${whatsappNumber.value}`;
+    fetch(url)
+      .then(response => response.blob())
+      .then(blob => {
+        previewImage.src = URL.createObjectURL(blob);
+        fileInput.value = null;
+      })
+      .catch(error => console.error(error));
+  } else {
+    previewImage.src = "";
   }
+}
 
-  // Update the picture preview with the WhatsApp image
-  function updatePictureWithWhatsAppImage(phoneNumber) {
-    var url = 'https://haproxy.inutil-labs.com/wspic/number=' + phoneNumber;
-    picturePreview.setAttribute('src', url);
+// Function to handle the file input
+function handleFileInput() {
+  const file = fileInput.files[0];
+  if (file) {
+    previewImage.src = URL.createObjectURL(file);
   }
+}
 
-  // Listen for changes in the phone number input
-  phoneInput.addEventListener('input', function() {
-    var phoneNumber = this.value;
-
-    // Show the WhatsApp checkbox if the phone number is valid
-    if (isPhoneNumberValid(phoneNumber)) {
-      whatsappCheckbox.parentNode.style.display = 'block';
-    } else {
-      whatsappCheckbox.parentNode.style.display = 'none';
-    }
-  });
-
-  // Listen for changes in the WhatsApp checkbox
-  whatsappCheckbox.addEventListener('change', function() {
-    if (this.checked) {
-      var phoneNumber = phoneInput.value;
-
-      // Check if the phone number is valid before updating the picture preview
-      if (isPhoneNumberValid(phoneNumber)) {
-        updatePictureWithWhatsAppImage(phoneNumber);
-        uploadButton.disabled = true;
-      } else {
-        alert('Please enter a valid phone number.');
-        this.checked = false;
-      }
-    } else {
-      picturePreview.setAttribute('src', '/img/bust-silhouette.png');
-      uploadButton.disabled = false;
-    }
-  });
-
-  // Listen for changes in the upload button
-  uploadButton.addEventListener('change', function() {
-    var file = this.files[0];
-
-    if (file && !whatsappCheckbox.checked) {
-      // Display the uploaded picture in the preview
-      var reader = new FileReader();
-      reader.onload = function(event) {
-        picturePreview.setAttribute('src', event.target.result);
-      };
-      reader.readAsDataURL(file);
-
-      // Enable the WhatsApp checkbox
-      whatsappCheckbox.disabled = false;
-    } else if (whatsappCheckbox.checked) {
-      // Check if the phone number is valid before updating the picture preview
-      var phoneNumber = phoneInput.value;
-      if (isPhoneNumberValid(phoneNumber)) {
-        updatePictureWithWhatsAppImage(phoneNumber);
-        uploadButton.disabled = true;
-      } else {
-        alert('Please enter a valid phone number.');
-        this.value = null;
-      }
-    } else {
-      // Reset the preview to the default bust silhouette
-      picturePreview.setAttribute('src', '/img/bust-silhouette.png');
-    }
-  });
-};
+// Add event listeners to the relevant elements
+whatsappCheckbox.addEventListener("change", handleWhatsappImage);
+whatsappNumber.addEventListener("input", handleWhatsappImage);
+fileInput.addEventListener("change", handleFileInput);
