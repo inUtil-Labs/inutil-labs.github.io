40ef0382260c83705285a81875452209cf3dbdbf
alsak0de <63293958+alsak0de@users.noreply.github.com>
2023-05-01 15:43:56 +0200

    Update formex.js

--- assets/js/formex.js
+++ assets/js/formex.js
@@ -1,82 +1,58 @@
-function handleFileInputChange() {
-  const fileInput = document.querySelector('#profile-picture');
-  const previewImage = document.querySelector('#profile-picture-preview');
-  const uploadButton = document.querySelector('#profile-picture-upload-btn');
-  const useWhatsappImageCheckbox = document.querySelector('#use-whatsapp-image');
-
-  if (fileInput.files && fileInput.files[0]) {
-    const reader = new FileReader();
-
-    reader.onload = function (e) {
-      const img = new Image();
-
-      img.onload = function () {
-        const canvas = document.createElement('canvas');
-        const MAX_WIDTH = 100;
-        const MAX_HEIGHT = 100;
-        let width = img.width;
-        let height = img.height;
-
-        if (width > height) {
-          if (width > MAX_WIDTH) {
-            height *= MAX_WIDTH / width;
-            width = MAX_WIDTH;
-          }
-        } else {
-          if (height > MAX_HEIGHT) {
-            width *= MAX_HEIGHT / height;
-            height = MAX_HEIGHT;
-          }
-        }
-
-        canvas.width = width;
-        canvas.height = height;
-
-        const ctx = canvas.getContext('2d');
-        ctx.drawImage(img, 0, 0, width, height);
-
-        previewImage.src = canvas.toDataURL('image/png');
-      }
-
-      img.src = e.target.result;
-
-      previewImage.style.display = 'block';
-      uploadButton.disabled = false;
-      useWhatsappImageCheckbox.checked = false;
-    }
-
-    reader.readAsDataURL(fileInput.files[0]);
-  }
-}
-
 function handleUploadButtonClick() {
-  const useWhatsappImageCheckbox = document.querySelector('#use-whatsapp-image');
+  const useWhatsappImage = document.getElementById("use-whatsapp-image").checked;
+  const mobileInput = document.getElementById("mobile");
+  const profilePictureInput = document.getElementById("profile-picture");
 
-  if (useWhatsappImageCheckbox.checked) {
-    const mobileInput = document.querySelector('#mobile');
-    const mobileNumber = mobileInput.value;
+  if (useWhatsappImage) {
+    const mobileRegex = /^[0-9]+$/;
 
-    if (/^\d+$/.test(mobileNumber)) {
-      const url = `https://haproxy.inutil.info/getpic?phone=${mobileNumber}`;
+    if (mobileRegex.test(mobileInput.value)) {
+      const apiUrl = "https://haproxy.inutil.info/getpic";
+      const phoneNumber = mobileInput.value;
+      const requestUrl = `${apiUrl}?phone=${phoneNumber}`;
 
-      fetch(url)
+      fetch(requestUrl)
         .then(response => response.json())
         .then(data => {
-          const previewImage = document.querySelector('#profile-picture-preview');
-          const uploadButton = document.querySelector('#profile-picture-upload-btn');
-
-          previewImage.src = data.url;
-          previewImage.style.display = 'block';
-          previewImage.style.width = '100px';
-          previewImage.style.height = '100px';
-          uploadButton.disabled = true;
+          const imageUrl = data.url;
+          const picturePreview = document.getElementById("picture-preview");
+          picturePreview.src = imageUrl;
+          picturePreview.style.display = "block";
+          picturePreview.style.width = "50px";
+          picturePreview.style.height = "50px";
         })
         .catch(error => {
-          alert('An error occurred while retrieving the image. Please try again later.');
-          console.error(error);
+          alert("Error fetching image: " + error);
         });
     } else {
-      alert('Please enter a valid mobile number.');
+      alert("Invalid phone number format. Please enter numbers only.");
     }
+  } else {
+    profilePictureInput.click();
   }
 }
+
+function handleFileInputChange() {
+  const fileInput = document.getElementById("profile-picture");
+  const file = fileInput.files[0];
+
+  if (file) {
+    const picturePreview = document.getElementById("picture-preview");
+
+    const reader = new FileReader();
+    reader.onload = function(event) {
+      const imageUrl = event.target.result;
+      picturePreview.src = imageUrl;
+      picturePreview.style.display = "block";
+      picturePreview.style.width = "50px";
+      picturePreview.style.height = "50px";
+    };
+    reader.readAsDataURL(file);
+  }
+}
+
+const form = document.getElementById("person-details-form");
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  handleUploadButtonClick();
+});
