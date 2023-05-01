6feaa07a683d5fd030719cdaf534bf6173c63f32
alsak0de <63293958+alsak0de@users.noreply.github.com>
2023-05-01 14:54:44 +0200

    Update formex.js

--- assets/js/formex.js
+++ assets/js/formex.js
@@ -1,115 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const form = document.getElementById('person-details-form');
-  const fileInput = document.getElementById('profile-picture');
-  const previewContainer = document.getElementById('profile-picture-preview');
-  const previewImage = previewContainer.querySelector(".profile-picture-preview__image");
-  const uploadButton = document.getElementById('profile-picture-upload-btn');
-  const whatsappCheckbox = document.getElementById('use-whatsapp-image');
-  const mobileInput = document.getElementById('mobile');
-  const uploadImageButton = document.getElementById('upload-image-btn');
+function handleUploadButtonClick() {
+  const useWhatsappImageCheckbox = document.querySelector('#use-whatsapp-image');
+  const mobileInput = document.querySelector('#mobile');
+  const uploadBtn = document.querySelector('#profile-picture-upload-btn');
+  const profilePicturePreview = document.querySelector('#profile-picture-preview');
   
-  function validateMobile(mobile) {
-    const regex = /^[0-9]{10}$/;
-    return regex.test(mobile);
-  }
-  
-  function toggleUploadButton(disabled) {
-    uploadButton.disabled = disabled;
-    uploadButton.style.backgroundColor = disabled ? "#D3D3D3" : "#007bff";
-    uploadButton.style.cursor = disabled ? "not-allowed" : "pointer";
-  }
-  
-  function showErrorMsg(element, message) {
-    element.innerText = message;
-    element.style.display = 'block';
-  }
-  
-  function hideErrorMsg(element) {
-    element.innerText = '';
-    element.style.display = 'none';
-  }
-  
-  function readImage(file) {
-    if (file.type && !file.type.startsWith('image/')) {
+  if (useWhatsappImageCheckbox.checked) {
+    const phoneNumber = mobileInput.value.replace(/\D/g, ''); // remove non-digits from phone number
+    
+    if (phoneNumber.length !== 10) {
+      alert('Please enter a valid 10-digit phone number.');
       return;
     }
     
-    const reader = new FileReader();
-    
-    reader.addEventListener('load', function() {
-      previewImage.setAttribute('src', this.result);
-    });
-    
-    reader.readAsDataURL(file);
-  }
-  
-  function uploadProfilePicture(file) {
-    const xhr = new XMLHttpRequest();
-    const formData = new FormData();
-    formData.append('file', file);
-    xhr.open('POST', 'https://haproxy.inutil-labs.com/wspic/number=' + mobileInput.value);
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        const response = JSON.parse(xhr.responseText);
-        if (response.success) {
-          previewImage.setAttribute('src', response.image);
-        } else {
-          alert(response.error);
-        }
-      } else {
-        alert('Failed to upload image.');
-      }
-    };
-    xhr.send(formData);
-  }
-  
-  function handleFileInputChange() {
-    const file = fileInput.files[0];
-    if (file) {
-      readImage(file);
-      toggleUploadButton(false);
-    } else {
-      previewImage.setAttribute('src', '');
-      toggleUploadButton(true);
-    }
-  }
-  
-  function handleUploadButtonClick() {
-    if (whatsappCheckbox && whatsappCheckbox.checked && validateMobile(mobileInput.value)) {
-      const xhr = new XMLHttpRequest();
-      xhr.open('GET', 'https://haproxy.inutil-labs.com/wspic/number=' + mobileInput.value);
-      xhr.onload = function() {
-        if (xhr.status === 200) {
-          const response = JSON.parse(xhr.responseText);
-          if (response.success) {
-            previewImage.setAttribute('src', response.image);
-          } else {
-            alert(response.error);
-          }
-        } else {
-          alert('Failed to fetch image.');
-        }
-      };
-      xhr.send();
-    } else {
-      const file = fileInput.files[0];
-      if (file) {
-        uploadProfilePicture(file);
-      }
-    }
-  }
-  
-  function handleMobileInputChange(event) {
-  const inputField = event.target;
-  const inputValue = inputField.value.trim();
-  const isValidMobile = /^\d{10}$/.test(inputValue);
-
-  console.log("Input value: ", inputValue);
-
-  if (isValidMobile) {
-    whatsappCheckbox.disabled = false;
-    alert("Valid mobile number!");
+    fetch(`https://haproxy.inutil.info/getpic?phone=${phoneNumber}`)
+      .then(response => response.json())
+      .then(data => {
+        const imageUrl = data.url;
+        profilePicturePreview.setAttribute('src', imageUrl);
+      })
+      .catch(error => console.error(error));
   } else {
-    whatsappCheckbox.disabled = true;
+    uploadBtn.disabled = false;
+    uploadBtn.click();
+    uploadBtn.disabled = true;
+    const uploadedImage = uploadBtn.previousSibling.previousSibling.files[0];
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      profilePicturePreview.setAttribute('src', e.target.result);
+    };
+    reader.readAsDataURL(uploadedImage);
   }
 }
+
