513586d4c299eb577da23653383ad7ce4175e0c1
alsak0de <63293958+alsak0de@users.noreply.github.com>
2023-05-02 19:25:03 +0200

    Update formex.js

--- assets/js/formex.js
+++ assets/js/formex.js
@@ -45,11 +45,11 @@ function handleUploadButtonClick() {
 }
 */
 
-
 function handleUploadButtonClick() {
   const useWhatsappImage = document.getElementById("use-whatsapp-image").checked;
   const mobileInput = document.getElementById("mobile");
   const profilePictureInput = document.getElementById("profile-picture");
+  const picturePreview = document.getElementById("profile-picture-preview");
 
   if (useWhatsappImage) {
     const mobileRegex = /^[0-9]+$/;
@@ -57,29 +57,24 @@ function handleUploadButtonClick() {
     if (mobileRegex.test(mobileInput.value)) {
       const apiUrl = "https://whatsapp-scraper.p.rapidapi.com/wspicture";
       const options = {
-	  method: 'GET',
-	  headers: {
-		'X-RapidAPI-Key': '7540839c98mshc8fe15e657db2c2p13c8ccjsnd1d2ebde022a',
-		'X-RapidAPI-Host': 'whatsapp-scraper.p.rapidapi.com'
-	  }
-    };
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key": "7540839c98mshc8fe15e657db2c2p13c8ccjsnd1d2ebde022a",
+          "X-RapidAPI-Host": "whatsapp-scraper.p.rapidapi.com",
+        },
+      };
       const phoneNumber = mobileInput.value;
       const requestUrl = `${apiUrl}?phone=${phoneNumber}`;
 
       fetch(requestUrl, options)
-  	.then(response => response.text())
-  	.then(data => {
-   	 const imageUrl = data.trim();
-    	const picturePreview = document.getElementById("profile-picture-preview");
-   	 picturePreview.src = imageUrl;
-   	 picturePreview.style.display = "block";
-   	 picturePreview.style.width = "50px";
-   	 picturePreview.style.height = "50px";
- 	 })
- 	 .catch(error => {
-	    alert("Error fetching image: " + error);
-	  });
-
+        .then((response) => response.text())
+        .then((data) => {
+          const imageUrl = data.trim();
+          picturePreview.src = imageUrl;
+        })
+        .catch((error) => {
+          alert("Error fetching image: " + error);
+        });
     } else {
       alert("Invalid phone number format. Please enter numbers only.");
     }
@@ -88,28 +83,15 @@ function handleUploadButtonClick() {
   }
 }
 
-
-function handleFileInputChange() {
-  const fileInput = document.getElementById("profile-picture");
-  const file = fileInput.files[0];
-
-  if (file) {
-    const picturePreview = document.getElementById("profile-picture-preview");
-
-    const reader = new FileReader();
-    reader.onload = function(event) {
-      const imageUrl = event.target.result;
-      picturePreview.src = imageUrl;
-      picturePreview.style.display = "block";
-      picturePreview.style.width = "50px";
-      picturePreview.style.height = "50px";
-    };
-    reader.readAsDataURL(file);
-  }
-}
-
 const form = document.getElementById("person-details-form");
-form.addEventListener("submit", event => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  handleUploadButtonClick();
+  const useWhatsappImage = document.getElementById("use-whatsapp-image").checked;
+
+  if (useWhatsappImage) {
+    handleUploadButtonClick();
+  } else {
+    form.submit();
+  }
 });
+
