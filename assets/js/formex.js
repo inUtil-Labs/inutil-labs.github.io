72dd2b2b9b7679dff650e64fe64de802d5a926d0
alsak0de <63293958+alsak0de@users.noreply.github.com>
2023-05-02 20:21:07 +0200

    Update formex.js

--- assets/js/formex.js
+++ assets/js/formex.js
@@ -54,7 +54,6 @@ function handleUploadButtonClick() {
     const mobileRegex = /^[0-9]+$/;
 
     if (mobileRegex.test(mobileInput.value)) {
-      // Call API to get Whatsapp image
       const apiUrl = "https://whatsapp-scraper.p.rapidapi.com/wspicture";
       const options = {
         method: 'GET',
@@ -73,8 +72,8 @@ function handleUploadButtonClick() {
           const picturePreview = document.getElementById("profile-picture-preview");
           picturePreview.src = imageUrl;
           picturePreview.style.display = "block";
-          picturePreview.style.width = "240px";
-          picturePreview.style.height = "240px";
+          picturePreview.style.width = "50px";
+          picturePreview.style.height = "50px";
         })
         .catch(error => {
           alert("Error fetching image: " + error);
@@ -84,7 +83,6 @@ function handleUploadButtonClick() {
       alert("Invalid phone number format. Please enter numbers only.");
     }
   } else {
-    // Trigger file input click event to browse for image file
     profilePictureInput.click();
   }
 }
@@ -109,44 +107,32 @@ function handleFileInputChange() {
 }
 
 const form = document.getElementById("person-details-form");
-
 form.addEventListener("submit", event => {
   event.preventDefault();
+  handleUploadButtonClick();
+});
 
+const useWhatsappImageCheckbox = document.getElementById("use-whatsapp-image");
+useWhatsappImageCheckbox.addEventListener("change", event => {
+  const picturePreview = document.getElementById("profile-picture-preview");
+  const fileInput = document.getElementById("profile-picture");
+
+  if (useWhatsappImageCheckbox.checked) {
+    picturePreview.style.display = "none";
+    fileInput.style.display = "none";
+  } else {
+    picturePreview.style.display = "block";
+    fileInput.style.display = "block";
+  }
+});
+
+const profilePictureUploadButton = document.getElementById("profile-picture-upload");
+profilePictureUploadButton.addEventListener("click", event => {
   const useWhatsappImage = document.getElementById("use-whatsapp-image").checked;
 
-  if (useWhatsappImage) {
-    // Make HTTP request to submit form data along with image URL
-    const mobileInput = document.getElementById("mobile");
-    const imageUrl = document.getElementById("profile-picture-preview").src;
-    const formData = new FormData();
-    formData.append("name", document.getElementById("name").value);
-    formData.append("email", document.getElementById("email").value);
-    formData.append("mobile", mobileInput.value);
-    formData.append("profile-picture-url", imageUrl);
+  if (!useWhatsappImage) {
+    const profilePictureInput = document.getElementById("profile-picture");
+    profilePictureInput.click();
+  }
+});
 
-    fetch("/submit-form-with-image-url", {
-      method: "POST",
-      body: formData
-    })
-    .then(response => response.text())
-    .then(data => {
-      alert(data);
-    })
-    .catch(error => {
-      alert("Error submitting form: " + error);
-    });
-  } else {
-    // Make HTTP request to submit form data along with image file
-    const formData = new FormData(form);
-
-    fetch("/submit-form", {
-      method: "POST",
-      body: formData
-    })
-    .then(response => response.text())
-    .then(data => {
-      alert(data);
-    })
-    .catch(error => {
-      alert("
