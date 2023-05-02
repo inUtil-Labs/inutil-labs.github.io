88debf83ec45a63b9fe822e2e0edb64ad100e032
alsak0de <63293958+alsak0de@users.noreply.github.com>
2023-05-02 19:16:05 +0200

    Update formex.js

--- assets/js/formex.js
+++ assets/js/formex.js
@@ -1,3 +1,51 @@
+/*
+function handleUploadButtonClick() {
+  const useWhatsappImage = document.getElementById("use-whatsapp-image").checked;
+  const mobileInput = document.getElementById("mobile");
+  const profilePictureInput = document.getElementById("profile-picture");
+
+  if (!profilePictureInput.value && !useWhatsappImage) {
+    profilePictureInput.click();
+  } else if (useWhatsappImage) {
+    const mobileRegex = /^[0-9]+$/;
+
+    if (mobileRegex.test(mobileInput.value)) {
+      const apiUrl = "https://whatsapp-scraper.p.rapidapi.com/wspicture";
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': '7540839c98mshc8fe15e657db2c2p13c8ccjsnd1d2ebde022a',
+          'X-RapidAPI-Host': 'whatsapp-scraper.p.rapidapi.com'
+        }
+      };
+      const phoneNumber = mobileInput.value;
+      const requestUrl = `${apiUrl}?phone=${phoneNumber}`;
+
+      fetch(requestUrl, options)
+        .then(response => response.text())
+        .then(data => {
+          const imageUrl = data.trim();
+          const picturePreview = document.getElementById("profile-picture-preview");
+          picturePreview.src = imageUrl;
+          picturePreview.style.display = "block";
+          picturePreview.style.width = "50px";
+          picturePreview.style.height = "50px";
+        })
+        .catch(error => {
+          alert("Error fetching image: " + error);
+        });
+
+    } else {
+      alert("Invalid phone number format. Please enter numbers only.");
+    }
+  } else {
+    const form = document.getElementById("person-details-form");
+    form.submit();
+  }
+}
+*/
+
+
 function handleUploadButtonClick() {
   const useWhatsappImage = document.getElementById("use-whatsapp-image").checked;
   const mobileInput = document.getElementById("mobile");
